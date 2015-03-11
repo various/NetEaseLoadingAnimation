daad19c4d3cdc0fee567ea2eb643e13ec4976fda
Various <apple123@mail.ustc.edu.cn>
2015-03-11 17:25:12 +0800

    init

---
+++ NetEaseLoadingAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NetEaseLoadingAnimation
+//
+//  Created by Tim Geng on 3/11/15.
+//  Copyright (c) 2015 GF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
