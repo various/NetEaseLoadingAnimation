e9436fe725cfab045657b64727b4aaf8de8cfa39
Various <apple123@mail.ustc.edu.cn>
2015-03-13 14:16:46 +0800

    update

--- NetEaseLoadingAnimation/ViewController.m
+++ NetEaseLoadingAnimation/ViewController.m
@@ -15,41 +15,41 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
-    CABasicAnimation
+    
     [super viewDidLoad];
+    
     _layer = [CAShapeLayer layer];
-    [self.view.layer addSublayer:_layer];
-    _layer.strokeColor = [UIColor redColor].CGColor;
-    _layer.fillColor = [UIColor clearColor].CGColor;
-    _layer.lineWidth = 2.0;
-    UIBezierPath *path = [UIBezierPath bezierPathWithArcCenter:CGPointMake(100, 100) radius:100 startAngle:0 endAngle:M_PI * 2 - 0.1 clockwise:YES];
-    _layer.path = path.CGPath;
-    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"strokeEnd"];
-    animation.duration = 2.0;
-    animation.fromValue = [NSNumber numberWithFloat:0.0];
-    animation.toValue = [NSNumber numberWithFloat:1.0];
-    animation.delegate = self;
-    [_layer addAnimation:animation forKey:nil];
-    
     self.layer.frame = CGRectMake(100, 100, 200, 200);
+
+    [self.view.layer addSublayer:_layer];
+    _layer.strokeColor = [UIColor lightGrayColor].CGColor;
+    _layer.fillColor = [UIColor clearColor].CGColor;
+    _layer.lineWidth = 3.0;
+    UIBezierPath *path = [UIBezierPath bezierPathWithArcCenter:CGPointMake(100, 100) radius:50 startAngle:-(M_PI_4) endAngle:(M_PI * 2 - M_PI_4 ) * 2 clockwise:YES];
+    _layer.path = path.CGPath;
+    
+    CABasicAnimation *animationStokeEnd = [CABasicAnimation animationWithKeyPath:@"strokeEnd"];
+    animationStokeEnd.duration = 2.0;
+    animationStokeEnd.fromValue = [NSNumber numberWithFloat:0.0];
+    animationStokeEnd.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseOut];
+    animationStokeEnd.toValue = [NSNumber numberWithFloat:1.0];
+    
+    CABasicAnimation *animationStokeStart = [CABasicAnimation animationWithKeyPath:@"strokeStart"];
+    animationStokeStart.duration = 2.0;
+    animationStokeStart.fromValue = [NSNumber numberWithFloat:0.0];
+    animationStokeStart.toValue = [NSNumber numberWithFloat:1.0];
+    //animationStokeStart.timingFunction = [CAMediaTimingFunction functionWithName:kcam]
     
     
-    //self.layer.backgroundColor = [UIColor blueColor].CGColor;
+    CAAnimationGroup *animationGroup = [CAAnimationGroup animation];
+    animationGroup.animations = [NSArray arrayWithObjects:animationStokeEnd,animationStokeStart, nil];
+    animationGroup.duration = 2;
+    animationGroup.repeatCount = HUGE_VAL;
+    [_layer addAnimation:animationGroup forKey:nil];
     
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
--(void)animationDidStop:(CAAnimation *)anim finished:(BOOL)flag{
-    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
-    animation.repeatCount = 10;
-    animation.duration = 2.0;
-    animation.fromValue = @(0);
-    animation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
-    
-    animation.toValue = @(4 * M_PI);
-    [_layer addAnimation:animation forKey:nil];
-    
-}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
